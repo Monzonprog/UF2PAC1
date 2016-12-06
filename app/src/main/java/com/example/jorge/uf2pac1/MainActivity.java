43bd263a103b014af398bd01fd158695d328f0ef
monzonprog <monzon.prog@gmail.com>
2016-12-06 18:55:07 +0100

    Pruebas

--- app/src/main/java/com/example/jorge/uf2pac1/MainActivity.java
+++ app/src/main/java/com/example/jorge/uf2pac1/MainActivity.java
@@ -1,46 +1,45 @@
 package com.example.jorge.uf2pac1;
 
-
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.SeekBar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
-    public TextView songName;
+    public TextView nombre;
+    private AnimationDrawable animacionRepro;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //set the layout of the Activity
         setContentView(R.layout.activity_main);
 
-        //initialize views
-        initializeViews();
-    }
-
-    public void initializeViews() {
-        songName = (TextView) findViewById(R.id.songName);
+        nombre = (TextView) findViewById(R.id.nombre);
         mediaPlayer = MediaPlayer.create(this, R.raw.lust_for_life);
-        songName.setText("Lust_For_Life.mp3");
+        nombre.setText("Lust For Life");
+
+        ImageView animacionRepro = (ImageView) findViewById(R.id.animacionView);
+        animacionRepro.setBackgroundResource(R.drawable.animacion);
 
     }
 
-    // play mp3 song
+
+    // Función para iniciar la reproducción
     public void play(View view) {
         mediaPlayer.start();
-
+        animacionRepro.start();
     }
 
-    // pause mp3 song
+    // Función para pausar la reproducción
     public void pause(View view) {
         mediaPlayer.pause();
+        animacionRepro.stop();
+
     }
 
 
